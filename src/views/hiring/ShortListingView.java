ed7e74ed23f7171708289c1097c3fc94edde4016
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-06 16:22:52 +0200

    shortlisting main method added

--- src/views/hiring/ShortListingView.java
+++ src/views/hiring/ShortListingView.java
@@ -1,2 +1,17 @@
-package views.hiring;public class ShortListingView {
+package views.hiring;
+
+import models.hiring.JobPosting;
+
+import java.util.ArrayList;
+
+public class ShortListingView {
+    public static void mainMethod() {
+        try{
+            System.out.println("SELECT THE JOB POST YOU WANT TO SHOT LIST FOR");
+            ArrayList<JobPosting> jobPostings = new ArrayList<JobPosting>();
+            jobPostings = JobPostingView.getJobPosts();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
