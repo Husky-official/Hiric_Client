3731a9dc5e6952c77f56a783c841fbdae522e248
Gashugi Aderline <aderlinecarmella@gmail.com>
2022-03-07 07:51:45 +0200

    pull origin main

--- src/models/hiring/JobPosting.java
+++ src/models/hiring/JobPosting.java
@@ -3,28 +3,79 @@ package models.hiring;
 import java.sql.*;
 
 public class JobPosting {
-    private Integer id;
-    private Integer jobId;
-    private Integer userId;
-    private String jobTitle;
-    private String jobDesc;
-    private String jobRequirements;
-    private String location;
-    private Date startDate;
-    private Time duration;
-    private Integer salary;
+    public Integer id;
+    public Integer jobId;
+    public Integer userId;
+    public String jobDesc;
+    public String jobRequirements;
+    public Integer location;
+    public Date startDate;
+    public Time startTime;
+    public Integer duration;
+    public Integer salary;
+    public String salaryType;
 
-    public JobPosting(Integer id,Integer jobId, Integer userId, String jobTitle, String jobDesc, String jobRequirements, String location, Date startDate, Time duration, Integer salary) {
+    public String getSalaryType() {
+        return salaryType;
+    }
+
+    public void setSalaryType(String salaryType) {
+        this.salaryType = salaryType;
+    }
+
+    public Integer getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(Integer workers) {
+        this.workers = workers;
+    }
+
+    public Integer getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(Integer paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer workers;
+    public Integer paymentStatus;
+    public String status;
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+
+
+    public JobPosting(Integer id,Integer jobId, Integer userId, String jobDesc, String jobRequirements, Integer location, Date startDate, Time startime, Integer duration, Integer salary, String salaryType, Integer workers, Integer paymentStatus, String status) {
         this.id = id;
         this.jobId = jobId;
         this.userId = userId;
-        this.jobTitle = jobTitle;
         this.jobDesc = jobDesc;
         this.jobRequirements = jobRequirements;
         this.location = location;
         this.startDate = startDate;
+        this.startTime = startime;
         this.duration = duration;
         this.salary = salary;
+        this.salaryType = salaryType;
+        this.workers = workers;
+        this.paymentStatus = paymentStatus;
+        this.status = status;
     }
 
     public JobPosting() {
@@ -54,14 +105,6 @@ public class JobPosting {
         this.userId = userId;
     }
 
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
     public String getJobDesc() {
         return jobDesc;
     }
@@ -78,11 +121,11 @@ public class JobPosting {
         this.jobRequirements = jobRequirements;
     }
 
-    public String getLocation() {
+    public Integer getLocation() {
         return location;
     }
 
-    public void setLocation(String location) {
+    public void setLocation(Integer location) {
         this.location = location;
     }
 
@@ -94,11 +137,11 @@ public class JobPosting {
         this.startDate = startDate;
     }
 
-    public Time getDuration() {
+    public Integer getDuration() {
         return duration;
     }
 
-    public void setDuration(Time duration) {
+    public void setDuration(Integer duration) {
         this.duration = duration;
     }
 
@@ -118,7 +161,6 @@ public class JobPosting {
         return "JobPosting{" +
                 "jobId=" + jobId +
                 "userId=" + userId +
-                ", jobTitle=" + jobTitle +
                 ", jobDescription='" + jobDesc + '\'' +
                 ", jobRequirements=" + jobRequirements +
                 ", location=" + location +
