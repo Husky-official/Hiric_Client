59b980e3cf775b9ab6df387e5c26e5df51197478
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-03-07 09:31:39 +0200

    fit: billing view

--- src/views/BillingView.java
+++ src/views/BillingView.java
@@ -8,9 +8,13 @@ import interfaces.MessageTypes;
 import models.Payment;
 import models.RequestBody;
 import models.User;
+import models.hiring.JobPosting;
 
+import java.io.DataInput;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -18,14 +22,17 @@ import java.util.concurrent.TimeUnit;
 import static utils.MessagePrinter.printConsoleMessage;
 
 public class BillingView {
-    public String paymentMethod(Double amount) throws Exception {
-        String returnString = "";
+    public Double jobSalary;
+    public Long empId;
+
+    public Integer paymentMethod(Long jobId) throws Exception {
+        Integer returnString = 1;
         Scanner scan = new Scanner(System.in);
 //        User payer = new User("hfdjshfsadjf","aldo@aldo.com");
 //        User payee = new User("jfdksjfksf","jabes@jabes.com");
-        Double money_to_pay = amount;
+        Long parsedJobId = jobId;
 //        PayObject payObject = new PayObject(payee,payer,money_to_pay);
-        printConsoleMessage(MessageTypes.NORMAL, false, "||  You must pay "+money_to_pay+"||");
+        printConsoleMessage(MessageTypes.NORMAL, false, "||  You must pay for job with id "+parsedJobId+"||");
         printConsoleMessage(MessageTypes.NORMAL,false,"choose your payment method");
         printConsoleMessage(MessageTypes.NORMAL,false,"1.Pay using MoMo ");
         printConsoleMessage(MessageTypes.NORMAL,false,"2.Pay Using paypal");
@@ -39,7 +46,7 @@ public class BillingView {
             printConsoleMessage(MessageTypes.NORMAL,false,"Enter your PIN: ");
             String PIN = scan.nextLine();
             //due to it being a console version we can't use API since they require a web interface
-            returnString = "MobileMoney";
+            returnString = 1;
         }else if(choice == 2){
             printConsoleMessage(MessageTypes.NORMAL,false,"Enter your email: ");
             scan.nextLine();
@@ -47,11 +54,11 @@ public class BillingView {
             printConsoleMessage(MessageTypes.NORMAL,false,"Enter your password: ");
             String paypal_password = scan.nextLine();
             //due to it being a console version we can't use API since they require a web interface
-            returnString = "PayPal";
+            returnString = 2;
         }else {
             printConsoleMessage(MessageTypes.NORMAL, false, "Invalid input !");
             TimeUnit.SECONDS.sleep(3);
-            paymentMethod(amount);
+            paymentMethod(jobId);
         }
         return returnString;
     }
@@ -61,38 +68,28 @@ public class BillingView {
         printConsoleMessage(MessageTypes.NORMAL, false, "\tPAY FOR THE JOB YOU'VE GIVEN");
         printConsoleMessage(MessageTypes.NORMAL, false,"\t-----------------------");
         printConsoleMessage(MessageTypes.NORMAL, false,"\tList of jobs given: ");
-        getListOfJobs();
+        String listOfJobs = getListOfJobs();
+        if (listOfJobs.equals("")) {
+            printConsoleMessage(MessageTypes.NORMAL, false,"\tYou never given a job in order to pay employees, first post a job ");
+            return;
+        }
+
         printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter the chosen id for the job: ");
         String inJobId = scanner.nextLine();
         Long jobId = Long.parseLong(inJobId);
 
-        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter the original amount for the job: ");
-        String inOgAmount = scanner.nextLine();
-        Double originalAmount = Double.parseDouble(inOgAmount);
+        Integer chosenPaymentMethod = paymentMethod(jobId);
 
-        String chosenPaymentMethod = paymentMethod(originalAmount);
-
-        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter the amount deducted from employee if any. If no, enter 0: ");
-        String inReducedAmount = scanner.nextLine();
-        Double reducedAmount = Double.parseDouble(inReducedAmount);
-
-        LocalDate initDateOfPay = LocalDate.now();
-        String dateOfPayment = initDateOfPay.toString();
         printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter the employee's id to pay: ");
         String inEmployeeId = scanner.nextLine();
         Long employeeId = Long.parseLong(inEmployeeId);
-        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your id as employer: ");
-        String inEmployerId = scanner.nextLine();
-        Long employerId = Long.parseLong(inEmployerId);
 
         Payment payment = new Payment();
         payment.setJobId(jobId);
-        payment.setOriginalAmount(originalAmount);
         payment.setPaymentMethod(chosenPaymentMethod);
-        payment.setReducedAmount(reducedAmount);
-        payment.setDateOfPayment(dateOfPayment);
         payment.setEmployeeId(employeeId);
-        payment.setEmployerId(employerId);
+        payment.setOriginalAmount(jobSalary);
+        payment.setEmployerId(empId);
 
         RequestBody requestBody = new RequestBody();
         requestBody.setUrl("/payment");
@@ -121,13 +118,12 @@ public class BillingView {
 
     public String getListOfJobs() throws IOException {
         RequestBody requestBody = new RequestBody();
-        requestBody.setUrl("/payroll");
+        requestBody.setUrl("/payment");
         requestBody.setAction("listJobs");
         User user = new User();
-        user.setEmail("benon@gmail.com");
-        user.setPassword("benon");
+        user.setEmail("abiheloaf@gmail.com");
+        user.setPassword("pass123");
         requestBody.setObject(user);
-        System.out.println(user.toString());
 
         String requestString = new ObjectMapper().writeValueAsString(requestBody);
 
@@ -136,8 +132,39 @@ public class BillingView {
 
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonResponse = objectMapper.readTree(response);
-        System.out.println(response);
-        return jsonResponse.asText();
+
+        String jobPostingList = String.valueOf(jsonResponse.get("object"));
+        int status = jsonResponse.get("status").asInt();
+        String message = jsonResponse.get("message").asText();
+        String actionDone = jsonResponse.get("actionToDo").asText();
+
+        if (message.equals("NOT FOUND ERROR")) {
+            printConsoleMessage(MessageTypes.NORMAL, false,"========================================================================");
+            printConsoleMessage(MessageTypes.NORMAL, false,"STATUS ||         MESSAGE        ||             ACTION DON              ");
+            printConsoleMessage(MessageTypes.NORMAL, false,"========================================================================");
+            printConsoleMessage(MessageTypes.NORMAL, false,status+"    ||" + message +"   ||" + actionDone);
+            printConsoleMessage(MessageTypes.NORMAL, false,"========================================================================");
+            return "";
+        }
+
+        String[] strRes = jobPostingList.split("JobPosting");
+        List<JobPosting> jobPostings = new ArrayList<JobPosting>();
+        for (int i = 1; i<strRes.length; i++) {
+//            jobPostings.add();
+            String jobPostStr = strRes[i].split("}")[0] + "}";
+            String[] fields = jobPostStr.split(", ");
+            String jobId = fields[0].split("=")[1];
+            String employerId = fields[1].split("=")[1];
+            empId = Long.valueOf(employerId);
+            String jobDescBefore = fields[3].split("=")[1].substring(1);
+            String jobDesc = jobDescBefore.substring(0, jobDescBefore.length() -1);
+            String salary = fields[8].split("=")[1].replace("}", "");
+            jobSalary = Double.valueOf(salary);
+
+            System.out.println("\t" + jobId + ". \t" + jobDesc + "\t" + salary);
+        }
+
+        return Arrays.toString(jobPostingList.split("JobPosting"));
     }
 
 }
