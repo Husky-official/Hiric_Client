1af6dbbbf2dbf195f41cc88ac125c4b0ccbbe488
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-03-14 10:09:28 +0200

    user Validations

---
+++ src/utils/registration/Validations.java
@@ -0,0 +1,86 @@
+/**
+ * @author UWENAYO Alain Pacifique
+ * @description This class is used to validate the user input
+ */
+package utils.registration;
+
+import java.io.NotActiveException;
+import java.util.Date;
+
+/** The type Validations. */
+public class Validations {
+  /**
+   * In range string.
+   *
+   * @param number the number
+   * @param min the min
+   * @param max the max
+   * @return the string
+   */
+  public static String inRange(int number, int min, int max) {
+        if(number > min && number < max) return number + " must be in range [" + min + "," + max + "]";
+        return "OK";
+    }
+  /**
+   * Is email valid string.
+   *
+   * @param email the email
+   * @return the string
+   */
+  public static String isEmailValid(String email) {
+        if(!email.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")) return "Email is not valid, must be in format: example.domain.extension";
+        return "OK";
+    }
+  /**
+   * Is password valid string.
+   *
+   * @param password the password
+   * @return the string
+   */
+  public static String isPasswordValid(String password) {
+        if(!password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) return "Password is not valid, must contain at least one number, one uppercase letter, one lowercase letter, one special character and at least 8 characters";
+        return "OK";
+    }
+
+  /**
+   * Is date of birth valid string.
+   *
+   * @param dateOfBirth the date of birth
+   * @return the string
+   */
+  public static String isDateOfBirthValid(Date dateOfBirth) {
+        Date eighteenYearsAgo = new Date(new Date().getTime() - (16L * 365 * 24 * 60 * 60 * 1000));
+        if(dateOfBirth.after(eighteenYearsAgo)) return "Date of birth is not valid, must be at least 16 years old";
+        return "OK";
+    }
+
+  /**
+   * Is date valid string.
+   *
+   * @param date the date
+   * @return the string
+   */
+  public static String isDateValid(String date) {
+        if(!date.matches("^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$")) return "Date is not valid, must be in format dd/mm/yyyy";
+        return "OK";
+    }
+  /**
+   * Is name valid string.
+   *
+   * @param name the name
+   * @return the string
+   */
+  public static String isNameValid(String name) {
+        if(!name.matches("^[a-zA-Z ]+$")) return "Name is not valid, must be in format: Uwenayo Alain Pacifique";
+        return "OK";
+    }
+  /**
+   * Not null boolean.
+   *
+   * @param value the value
+   * @return the boolean
+   */
+  public static boolean NotNull(String value) {
+        return !(value == null || value.isEmpty());
+    }
+}
--- src/views/RegisterView.java
+++ src/views/RegisterView.java
@@ -12,7 +12,8 @@ import models.RegisterUser;
 import models.RequestBody;
 import models.UserUtils.UserGender;
 import models.UserUtils.UserRoles;
-import utils.Handlers;
+import utils.registration.Handlers;
+import utils.registration.Validations;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,45 +21,74 @@ import java.util.Scanner;
 
 import static utils.MessagePrinter.printConsoleMessage;
 
-/**
- * The type RegisterUser view.
- */
+/** The type RegisterUser view. */
 public class RegisterView {
 
-    /**
-     * Register user.
-     *
-     * @throws Exception the exception
-     * @description register a new user
-     * @author UWENAYO ALain Pacifique
-     */
-    public static void registerUser() throws Exception {
+  /**
+   * Register user.
+   *
+   * @throws Exception the exception
+   * @description register a new user
+   * @author UWENAYO ALain Pacifique
+   */
+  public static void registerUser() throws Exception {
 
         Scanner scanner = new Scanner(System.in);
         UserRoles userRole = UserRoles.EMPLOYER;
+        String firstName;
+        String lastName = "";
         UserGender userGender = UserGender.MALE;
         printConsoleMessage(MessageTypes.NORMAL, false, "\tUSER REGISTER");
         printConsoleMessage(MessageTypes.NORMAL, false,"\t-----------------------");
-        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your first name[ex:UWENAYO]");
-        String firstName = scanner.nextLine();
-        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your last name[ex:Alain]");
-        String lastName = scanner.nextLine();
-        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your gender[1:Male,2:Female]");
-        if(scanner.nextInt() == 2){
-            userGender = UserGender.FEMALE;
+        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your first name[ex:UWENAYO ALain Pacifique]");
+        firstName = scanner.nextLine();
+        while(!Validations.isNameValid(firstName).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR, false, Validations.isNameValid(firstName));
+            System.out.println(firstName);
         }
+        int genderChoice = scanner.nextInt();
+        printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your gender[1:Male,2:Female]");
+        while(Validations.inRange(genderChoice,1,2).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR,false,Validations.inRange(genderChoice,1,2));
+            genderChoice = scanner.nextInt();
+        }
+        if(genderChoice == 2) userGender = UserGender.FEMALE;
+        // solving the know bug for using nextInt()
         scanner.nextLine();
         printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your email[ex:example@domain.com]");
         String email = scanner.nextLine();
+        while(!Validations.isEmailValid(email).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR, false, Validations.isEmailValid(email));
+            email = scanner.nextLine();
+        }
         printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your password[strong password suggested]");
         String password = scanner.nextLine();
+        while(!Validations.isPasswordValid(password).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR, false, Validations.isPasswordValid(password));
+            password = scanner.nextLine();
+        }
         printConsoleMessage(MessageTypes.NORMAL, false,"\tEnter your Date of Birth[ex:01/12/1990]");
+        String inputDOB = scanner.nextLine();
+        while(!Validations.isDateValid(inputDOB).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR, false, Validations.isDateValid(inputDOB));
+            inputDOB = scanner.nextLine();
+        }
         Date dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(scanner.nextLine());
+        while(!Validations.isDateOfBirthValid(dateOfBirth).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR, false, Validations.isDateOfBirthValid(dateOfBirth));
+            dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(scanner.nextLine());
+        }
         printConsoleMessage(MessageTypes.NORMAL, false,"\tChoose account type[1:employer,2:employee]");
         int accountType = scanner.nextInt();
+        while(Validations.inRange(accountType,1,2).equals("OK")){
+            printConsoleMessage(MessageTypes.ERROR,false,Validations.inRange(accountType,1,2));
+            accountType = scanner.nextInt();
+        }
         if (accountType == 2){
             userRole = UserRoles.EMPLOYEE;
         }
+        if(firstName.contains(" ")) lastName = firstName.substring(firstName.indexOf(" ")+1);
+        firstName = firstName.substring(0,firstName.indexOf(" "));
         RegisterUser user = new RegisterUser(1,firstName,lastName,email,password,userGender,userRole, dateOfBirth);
         RequestBody requestBody = new RequestBody();
         requestBody.setUrl("/users");
@@ -71,4 +101,3 @@ public class RegisterView {
     }
 
 }
-
