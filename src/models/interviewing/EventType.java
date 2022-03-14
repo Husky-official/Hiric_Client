b1b94af6e9d6945511367c0999e7383e4acdd3af
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-03-14 07:39:35 +0200

    eventScheduling updated

---
+++ src/models/interviewing/EventType.java
@@ -0,0 +1,11 @@
+package models.interviewing;
+
+/**
+ * @author I_Clarisse
+ * @description event type enums
+ */
+public enum EventType {
+    CALL,
+    ONLINE_INTERVIEW,
+    IN_PERSON_INTERVIEW
+}
--- src/views/interviewing/EventSchedulingView.java
+++ src/views/interviewing/EventSchedulingView.java
@@ -6,16 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import interfaces.MessageTypes;
 import models.Message;
 import models.RequestBody;
+import models.hiring.JobPosting;
 import models.interviewing.EventScheduling;
+import models.interviewing.EventType;
 import utils.ExitApplication;
 import utils.Loader;
 import utils.MessagePrinter;
+import views.hiring.JobPostingView;
 
 import java.io.IOException;
-import java.sql.Time;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 import static utils.MessagePrinter.printConsoleMessage;
@@ -30,33 +30,31 @@ public class EventSchedulingView {
 
         MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t EVENT SCHEDULING");
         MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t ----------------");
-        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 1. Schedule event");
-        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 2. Get all scheduled events");
-        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 3. Go Back");
+        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 1. Schedule New Event");
+        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 2. Get All Scheduled Events");
+        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 3. Add Participants");
+        MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t 4. Go Back");
         MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, true, "\t\t Enter choice: ");
         eventScheduleChoice = scanner.nextInt();
 
         try{
             switch (eventScheduleChoice){
-                case 1:
-                    ScheduleEvent();
-                    break;
-                case 2:
-                    getAllScheduledEvents();
-                    break;
-                case 3:
+                case 1 -> ScheduleEvent();
+                case 2 -> getAllScheduledEvents();
+                case 3 -> addParticipant();
+                case 4 -> {
                     new Loader(15, "Going back");
                     MessagePrinter.skipLines(1);
-                    break;
-                case 0:
-                    new ExitApplication();
-                    break;
-                default:
+                }
+
+                case 0 -> new ExitApplication();
+                default -> {
                     MessagePrinter.skipLines(1);
                     MessagePrinter.printConsoleMessage(MessageTypes.ERROR, false, "Incorrect choice! Try again...");
                     new Loader(20, "Retrying");
                     MessagePrinter.skipLines(2);
                     mainMethod();
+                }
             }
         } catch (Exception e) {
             MessagePrinter.printConsoleMessage(MessageTypes.ERROR, false, e.getMessage());
@@ -64,19 +62,38 @@ public class EventSchedulingView {
             mainMethod();
         }
     }
-
+    // choose from existing Jobposts and schedule an event
     public static void ScheduleEvent() throws IOException, InterruptedException, ParseException {
         try {
             Scanner scanner = new Scanner(System.in);
-
+            EventType eventType = EventType.CALL;
             MessagePrinter.skipLines(1);
             MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t PROVIDE THE FOLLOWING INFORMATION TO SCHEDULE AN INTERVIEW");
             MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t --------------------------------------------------------------------");
+            printConsoleMessage(MessageTypes.NORMAL, false, "\t\t CHOOSE THE JOB POST YOU WANT TO SCHEDULE AN EVENT FOR");
+
+            JobPosting[] jobPostings;
+            jobPostings = JobPostingView.getJobPosts();
+            for(int i = 0; i < jobPostings.length; i++) {
+                System.out.println(jobPostings[i].id + " : " + jobPostings[i].jobDesc);
+            }
+
+            printConsoleMessage(MessageTypes.NORMAL, false, "\t\t Enter Job Post ID: ");
+            String jobId = scanner.nextLine();
+            Integer jobPostId = Integer.parseInt(jobId);
+
             MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t Event Name: ");
             String eventName = scanner.nextLine();
             MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t Choose event type(call, online interview, in-person interview):");
-            String eventType = scanner.nextLine();
-
+            printConsoleMessage(MessageTypes.NORMAL, false,"\t\t 1.CALL");
+            printConsoleMessage(MessageTypes.NORMAL, false,"\t\t 2.ONLINE INTERVIEW");
+            printConsoleMessage(MessageTypes.NORMAL, false,"\t\t 1.IN-PERSON INTERVIEW");
+            if (scanner.nextInt() == 2){
+                eventType = EventType.ONLINE_INTERVIEW;
+            }else if(scanner.nextInt() == 3){
+                eventType = EventType.IN_PERSON_INTERVIEW;
+            }
+            scanner.nextLine();
             MessagePrinter.printConsoleMessage(MessageTypes.NORMAL, false, "\t\t Enter the event date(dd/MM/yyyy): ");
             String eventDate = scanner.nextLine();
 //            Date eventDate = new SimpleDateFormat("dd/MM/yyy").parse(myDate);
@@ -90,12 +107,13 @@ public class EventSchedulingView {
 //            Time endTime = Time.valueOf(time2);
 
             EventScheduling eventScheduling = new EventScheduling();
+            eventScheduling.setJobPostId(jobPostId);
             eventScheduling.setEventName(eventName);
             eventScheduling.setEventType(eventType);
             eventScheduling.setEventDate(eventDate);
             eventScheduling.setStartTime(startTime);
             eventScheduling.setEndTime(endTime);
-            eventScheduling.setEventCreator(102);
+            eventScheduling.setEventCreator(1);
 
             RequestBody requestBody = new RequestBody();
             requestBody.setUrl("/eventScheduling");
@@ -114,11 +132,11 @@ public class EventSchedulingView {
             String message = jsonResponse.get("message").asText();
             String actionDone = jsonResponse.get("actionToDo").asText();
 
-            printConsoleMessage(MessageTypes.NORMAL, false, "========================================================================");
-            printConsoleMessage(MessageTypes.NORMAL, false, "STATUS ||         MESSAGE        ||             ACTION DON              ");
-            printConsoleMessage(MessageTypes.NORMAL, false, "========================================================================");
-            printConsoleMessage(MessageTypes.NORMAL, false, status + "    ||" + message + "   ||" + actionDone);
-            printConsoleMessage(MessageTypes.NORMAL, false, "========================================================================");
+            printConsoleMessage(MessageTypes.SUCCESS, false, "========================================================================");
+            printConsoleMessage(MessageTypes.SUCCESS, false, "STATUS ||         MESSAGE        ||             ACTION DONE             ");
+            printConsoleMessage(MessageTypes.SUCCESS, false, "========================================================================");
+            printConsoleMessage(MessageTypes.SUCCESS, false, status + "    ||" + message + "   ||" + actionDone);
+            printConsoleMessage(MessageTypes.SUCCESS, false, "========================================================================");
 
         }catch (Exception e){
             e.printStackTrace();
@@ -152,12 +170,15 @@ public class EventSchedulingView {
         String actionToDo = jsonResponse.get("actionToDo").asText();
 
         JsonNode jsonNode = objectMapper.readTree(String.valueOf(jsonResponse.get("object")));
-//        System.out.println(jsonNode);
         EventScheduling[] eventSchedules = objectMapper.treeToValue(jsonNode, EventScheduling[].class);
 
+        MessagePrinter.skipLines(1);
+        printConsoleMessage(MessageTypes.SUCCESS, false, "=================================================");
+        printConsoleMessage(MessageTypes.SUCCESS, false, "EVENT ID  || EVENT NAME ");
+        printConsoleMessage(MessageTypes.SUCCESS, false, "=================================================");
         for (int i = 0; i < eventSchedules.length; i ++){
-            printConsoleMessage(MessageTypes.NORMAL, false, "\t\t"+eventSchedules[i].getId()+"\t\t"+eventSchedules[i].getEventName());
-            System.out.println();
+            printConsoleMessage(MessageTypes.SUCCESS, false, "  "+eventSchedules[i].getId()+"       || "+eventSchedules[i].getEventName());
+            printConsoleMessage(MessageTypes.SUCCESS, false, "=================================================");
         }
 
         printConsoleMessage(MessageTypes.NORMAL, false,"========================================================================");
@@ -166,7 +187,16 @@ public class EventSchedulingView {
         printConsoleMessage(MessageTypes.NORMAL, false,status+"    ||" + message +"   ||" + actionToDo);
         printConsoleMessage(MessageTypes.NORMAL, false,"========================================================================");
         return eventSchedules;
+    }
 
+    public static void addParticipant() throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        printConsoleMessage(MessageTypes.NORMAL, false, "\t Add Participant");
+        printConsoleMessage(MessageTypes.NORMAL, false, "\t ----------------");
+        getAllScheduledEvents();
+        printConsoleMessage(MessageTypes.NORMAL, false, "Enter event id");
+        String eventId = scanner.nextLine();
+        Integer scheduleId = Integer.parseInt(eventId);
 
     }
 }
